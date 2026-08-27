4afb04b4b43c814f7d3ea65b527feed74b7c06e1
Henry Li <hl3547@princeton.edu>
2026-08-26 22:43:28 -0400

    Stop tracking node_modules

---
+++ shareware/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import "98.css";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div >
+
+      </div>
+    </>
+  )
+}
+
+export default App
